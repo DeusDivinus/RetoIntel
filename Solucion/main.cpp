b7f1bd044b3f880b2054ba6064b2e1b4922be840
Daniel <78000390+DeusDivinus@users.noreply.github.com>
2023-07-25 15:58:48 -0600

    Actualizar main.cpp

--- Solucion/main.cpp
+++ Solucion/main.cpp
@@ -2,35 +2,19 @@
 #include <omp.h>
 #include <ctime>
 #include <bits/stdc++.h>
+#include "primeNumbers.cpp"
+#include <list>
 using namespace std;
 
-bool isPrime(int n)
-{
-    if (n <= 1)
-        return false;
-
-    for (int i = 2; i <= sqrt(n); i++)
-        if (n % i == 0)
-            return false;
- 
-    return true;
-}
+int* primeNumbers[0];
 
 void print(string str){
     cout << str << endl;
 }
 
-void getPrimes(int n){
-    for(int i = 0; i < n; i++)
-    {
-        string boolresult = isPrime(i) ? "Primo" : "Normal";
-        string out = to_string(i)+" es "+boolresult;
-        print(out);
-    }
-}
-
 int main(int argc, char **argv){
-    getPrimes(1000);
+    getPrimes(*primeNumbers, 1000);
+    cout << *primeNumbers << endl;
     // for(int i = 0; i < 100; i++){
         
     // }
