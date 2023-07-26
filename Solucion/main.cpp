c6af1d1df952b6a8d72b62477ac3cf3832ff100d
Daniel <78000390+DeusDivinus@users.noreply.github.com>
2023-07-26 16:01:35 -0600

    Actualizar codigo de main.cpp
    Metodo de obtencion de numeros primos y conjentura fuerte

--- Solucion/main.cpp
+++ Solucion/main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
-#include "primeNumbers.cpp"
+#include "primeNumbers.h"
+#include "conjeturaFuerte.h"
 using namespace std;
 
 
 
 int main(){
-    int* numerosPrimos = getPrimes(100);
-    for( unsigned int a = 0; a < _length; a = a + 1 ){
-        cout << numerosPrimos[a] << endl;
+    // float* numerosPrimos = getPrimes(22222222);
+    // for( unsigned int a = 0; a < _length; a = a + 1 ){
+    // }
+    int maxlimit = 22222222;
+    int iterator;
+    for(; iterator = getPrimes(maxlimit);){
+        cout << iterator << endl;
+        strong(iterator, maxlimit);
     }
     return 0;
 }
