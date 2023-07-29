8deec2c58b48702ac6d2cbf221cde2f0f3759c7d
WickedDaniel <danielentity01@gmail.com>
2023-07-29 10:52:15 -0600

    Modificaciones a conjeturaFuerte, cambio de metodo de busqueda de pares, ademas actualizar main.cpp para la ejecucion

--- Solucion/conjeturaFuerte.h
+++ Solucion/conjeturaFuerte.h
@@ -1,30 +1,63 @@
 #include <iostream>
 #include <omp.h>
+#include <list>
 #include "primeNumbers.h"
 using namespace std;
  
-void getResult(int prime, int number)
+vector<int> pairs;
+vector<int> getResult(vector<int> primes, int number, int max, int newAt = 0)
 {
-    if(isPrime(number-prime))
-    {
-        
+    if (number > max){
+        cout << primes.at(newAt) << endl;
+        return pairs;
     }
+    for(int v: primes){
+        if(v < primes.at(newAt))
+        {
+            continue;
+        }
+
+        if(number % 7 == 0 && number % 3 == 0)
+        {
+            if(primes.at(newAt) < number/2)
+            {
+                newAt += 3;
+            }
+            newAt += 2;
+        }
+
+        for(int x: primes)
+        {
+            if(x > number)
+            {
+                break;   
+            }
+            int result = v+x;
+            if(result == number){;
+                pairs.push_back(result);
+                return getResult(primes, number+2, max, newAt);
+            }
+        }
+    }
+    return pairs;
 }
 
-void strong(int prime, int limit)
+void strong(vector<int> primes, int min, int max)
 {
-    for(; int i = getPrimes(limit);){
-        getResult(prime, i);
-    }
+    getResult(primes, 4, max);
+    // list<int> pairs(max);
+    // for(int v: primes){
+        // for(int x: primes)
+        // {
+            // int result = v+x;
+            // if(result % 2 == 0 && result <= max){;
+                // pairs.insert(pairs.begin()+result, 1, result);
+            // }
+        // }
+        // cout << v << endl;
+    // }
+    cout << pairs.size() << endl;
+    sort(pairs.begin(), pairs.end());
+    for(auto x : pairs)
+        cout << x << endl;
 }
-
-
-// int main(){
-//     for(int i = 0; i <= 22222222; i++){
-//         if(i == 22222222){
-//             cout << "22222222" << endl;
-//         };
-//     }
-//     cout << "ready" << endl;
-//     return 0;
-// }
--- Solucion/main.cpp
+++ Solucion/main.cpp
@@ -1,56 +1,40 @@
 #include <iostream>
 #include <fstream>
 #include <omp.h>
+#include <bits/stdc++.h>
 #include <pthread.h>
 #include <chrono>
 #include <vector>
 #include "primeNumbers.h"
 #include "conjeturaFuerte.h"
-
+#include "framework.h"
 using namespace std::chrono;
 using namespace std;
 
-class SW{
-    private:
-        high_resolution_clock::time_point start;
-        high_resolution_clock::time_point stop;
-    public:
-    void startSW(){
-        start = high_resolution_clock::now();
-    };
-    void stopSW(){
-        stop = high_resolution_clock::now();
-    };    
-    void showResult(){
-        stopSW();
-        auto duration = duration_cast<chrono::nanoseconds>(stop - start).count() * 1e-9;
-        cout << "Time taken by program is : " << fixed
-        << duration << setprecision(9);
-        cout << " sec" << endl;
-    }
-};
-
 // ofstream MyFile("filename.txt");
 // MyFile.close();
-int maxLimit = 222222 ;
-SW newSW;
+int maxLimit = 100000;
+SW mainSW;
+SW xSW;
+
 int main(){
-    newSW.startSW();
+    mainSW.startSW();
     ofstream MyFile("primes.txt");
     vector<int> primes;
-    int lastPrime = 2;
-    for(; int i = getPrimes(0, maxLimit);)
-    {
-        primes.insert(primes.begin()+primes.size(), 1, i);
-        // if(lastPrime && (lastPrime + i) % 2 == 0){
-        //     cout << lastPrime+i << '='
-        //     << lastPrime << '+'
-        //     << i << endl;
-        // }
-        lastPrime = i;
+    // for(; int i = getPrimes(0, maxLimit);)
+    // {
+    //     primes.insert(primes.begin()+primes.size(), 1, i);
+    // }
+    int i;
+    for(i = 0; i <= maxLimit; i++){
+        if(isPrime(i)){
+            primes.insert(primes.begin()+primes.size(), 1, i);
+        }
     }
+    strong(primes, 0, maxLimit);
 
+    cout << "Finished search\n" << "Size:" << primes.size() << endl;
     MyFile.close();
-    newSW.showResult();
+    mainSW.showResult();
     return 0;
 }
