788a6d6ad03a2f54e0b8dfe41f86439702570bb1
WickedDaniel <danielentity01@gmail.com>
2023-07-31 01:04:53 -0600

    update primeNumbers.h with optimizations - using vector<char> for performance - parallelization - all primes greater to 2 (x>2) are uneven, quicker iteration

--- Solucion/primeNumbers.h
+++ Solucion/primeNumbers.h
@@ -10,26 +10,30 @@ using namespace std;
 #ifndef primeNumbers_H
 #define primeNumbers_H
 SW pnSW;
-bool isPrime(int n) {
+char isPrime(int n) {
     if (n <= 1)
-        return false;
+        return 0;
     if (n <= 3)
-        return true;
+        return 1;
     if (n % 2 == 0 || n % 3 == 0)
         return false;
     for (int i = 5; i * i <= n; i += 6) {
         if (n % i == 0 || n % (i + 2) == 0)
             return false;
     }
-    return true;
+    return 1;
 }
 
-vector<bool> getPrimes(int min, int max)
+vector<char> getPrimes(int min, int max)
 {
-    vector<bool> primesBool(max + 1, true);
+    vector<char> primesBool(max + 1, 0);
+    primesBool[2] = 1;
     #pragma omp parallel for schedule(dynamic)
     for (int i = min; i <= max; i++) {
-        primesBool[i] = isPrime(i);
+        if(i%2 != 0)
+        {
+            primesBool[i] = isPrime(i);
+        }
     }
     return primesBool;
 }
