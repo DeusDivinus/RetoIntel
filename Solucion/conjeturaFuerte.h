3ab6f6173b8daafc1e5a37f46994a895edee2d8f
WickedDaniel <danielentity01@gmail.com>
2023-07-31 02:00:14 -0600

    update conjeturaFuerte.h - optimize the search algorithm or whatever - forgot to clean up debug output, lmao even - flex on the time it takes (went from ages to just 5 seconds on my pc) - changes made by me (Daniel)

--- Solucion/conjeturaFuerte.h
+++ Solucion/conjeturaFuerte.h
@@ -3,61 +3,55 @@
 #include <list>
 #include "primeNumbers.h"
 using namespace std;
- 
-vector<int> pairs;
-vector<int> getResult(vector<int> primes, int number, int max, int newAt = 0)
+
+bool getStrongNumber(int number, const vector<bool> primes)
 {
-    if (number > max){
-        cout << primes.at(newAt) << endl;
-        return pairs;
+    if(number%2!=0){
+        return false;
     }
-    for(int v: primes){
-        if(v < primes.at(newAt))
+    int sqrtNumber = static_cast<int>(sqrt(number));
+    for (int i = 2; i <= sqrtNumber; i++) {
+        if (primes[i] && primes[number - i]) {
+            cout << number << endl;
+            return true;
+        }
+    }
+    return false;
+}
+
+void checkPairsLeft(const vector<char>& pairs, unsigned int max) {
+    cout << "Pairs left to get: ";
+    unsigned int left = 0;
+    for (unsigned int i = 2; i <= max; i += 2) {
+        if(pairs[i] == 0) {
+            ++left;
+            cout << i << endl;
+        }
+    }
+    cout << left << endl;
+} 
+
+void strong(const vector<char>& primes, int min, int max) {
+    vector<char> pairs(max+1, 0);
+    unsigned int result;
+    pairs[2] = 1;
+    for (unsigned int i = 0; i <= max; ++i) {
+        result = i*2;
+        if (primes[i] & (result % 2 == 0) & result < max){
+            pairs[result] = 1;
+        }
+        if(i < 4 | i%2 != 0 | pairs[i])
         {
             continue;
         }
-
-        if(number % 7 == 0 && number % 3 == 0)
-        {
-            if(primes.at(newAt) < number/2)
+        for (unsigned int v = 0; v <= max; ++v){
+            result = i-v;
+            if(primes[v] & primes[result])
             {
-                newAt += 3;
-            }
-            newAt += 2;
-        }
-
-        for(int x: primes)
-        {
-            if(x > number)
-            {
-                break;   
-            }
-            int result = v+x;
-            if(result == number){;
-                pairs.push_back(result);
-                return getResult(primes, number+2, max, newAt);
+                pairs[result+v] = 1;
+                break;
             }
         }
     }
-    return pairs;
-}
-
-void strong(vector<int> primes, int min, int max)
-{
-    getResult(primes, 4, max);
-    // list<int> pairs(max);
-    // for(int v: primes){
-        // for(int x: primes)
-        // {
-            // int result = v+x;
-            // if(result % 2 == 0 && result <= max){;
-                // pairs.insert(pairs.begin()+result, 1, result);
-            // }
-        // }
-        // cout << v << endl;
-    // }
-    cout << pairs.size() << endl;
-    sort(pairs.begin(), pairs.end());
-    for(auto x : pairs)
-        cout << x << endl;
+    checkPairsLeft(pairs, max);
 }
